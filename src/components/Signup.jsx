1d6b5471a431634bbe7e265d49253c7d148d8c9d
Saumya Nayak <saumyo224@gmail.com>
2025-06-29 18:11:32 +0530

    implement signup form with validation and loading state

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { Input, Button } from "./index";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import authServices from "../appwrite/authServices.js";
+import toast, { Toaster } from "react-hot-toast";
+import { LoaderCircle } from "lucide-react";
+import { useState } from "react";
 
 function Signup() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [signUpError, setError] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      setIsLoading(true);
+      console.log(data);
+    } catch (error) {
+      setError(error);
+      console.log("Sign up error: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="border-2 border-gray-600 rounded-lg m-5 p-10">
       {/* Main heading */}
@@ -16,15 +41,115 @@ function Signup() {
       </div>
 
       {/* Registration form */}
-      <div className="my-5 ">
+      <div className="my-5 " onSubmit={handleSubmit(onSubmit)}>
         <form action="" className="space-y-3 w-full ">
-          <Input type="text" lable="Name" />
-          <Input type="email" lable="Email" />
-          <Input type="text" lable="Phone no" />
-          <Input type="password" lable="Password" />
-          <Input type="password" lable="Confirm password" />
-          <Button title="Register" type="submit" />
+          <Input
+            type="text"
+            lable="First Name"
+            placeholder="first name"
+            {...register("firstName", {
+              required: "Required",
+              minLength: {
+                value: 2,
+                message: "Must have atleast 2 characters",
+              },
+            })}
+          />
+          {errors.firstName && (
+            <p className="text-red-500"> {errors.firstName.message} </p>
+          )}
+          <Input
+            type="text"
+            lable="Last Name"
+            placeholder=" last name"
+            {...register("lastName", {
+              required: "Required",
+              minLength: {
+                value: 2,
+                message: "Must have alteast 2 characters",
+              },
+            })}
+          />
+          {errors.lastName && (
+            <p className="text-red-500">{errors.lastName.message}</p>
+          )}
+          <Input
+            type="text"
+            lable="Username"
+            placeholder="username"
+            {...register("username", {
+              required: "Required",
+              minLength: {
+                value: 3,
+                message: "Must have alteast 3 characters",
+              },
+            })}
+          />
+          {errors.username && (
+            <p className="text-red-500">{errors.username.message}</p>
+          )}
+          <Input
+            type="email"
+            lable="Email"
+            placeholder="email"
+            {...register("email", {
+              required: "Required",
+            })}
+          />
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+          <Input
+            type="text"
+            lable="Phone no"
+            placeholder="phone no"
+            {...register("phoneNo", {
+              required: "Required",
+              minLength: {
+                value: 10,
+                message: "Phone no less than 10 digits",
+              },
+            })}
+          />
+          {errors.phoneNo && (
+            <p className="text-red-500">{errors.phoneNo.message}</p>
+          )}
+          <Input
+            type="password"
+            lable="Password"
+            placeholder="password"
+            {...register("password", {
+              required: "Required",
+              minLength: {
+                value: 6,
+                message: "Must have alteast 6 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+          <Input
+            type="password"
+            lable="Confirm password"
+            placeholder="re-enter password"
+            {...register("confirmPassword", {
+              required: "Required",
+              minLength: {
+                value: 6,
+                message: "Must have alteast 6 characters",
+              },
+            })}
+          />
+          {errors.confirmPassword && (
+            <p className="text-red-500">{errors.confirmPassword.message}</p>
+          )}
+          <Button>
+            {" "}
+            {isLoading ? <LoaderCircle className="animate-spin" /> : "Register"}
+          </Button>
         </form>
+        {signUpError && <p className="text-red-500">{signUpError}</p>}
       </div>
       <p className="text-center text-lg">Already registered?</p>
 
