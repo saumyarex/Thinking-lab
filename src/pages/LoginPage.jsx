4a14d7abce623ffe717480947db5d57e8ee7b692
Saumya Nayak <saumyo224@gmail.com>
2025-06-29 17:17:06 +0530

    add LoginPage component and update SignupPage layout for consistent styling

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import { Container, Login } from "../components";
+
+function LoginPage() {
+  return (
+    <Container className={"flex flex-col justify-center items-center"}>
+      <Login />
+    </Container>
+  );
+}
+
+export default LoginPage;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,7 +2,7 @@ import { Container, Signup } from "../components";
 
 function SignupPage() {
   return (
-    <Container>
+    <Container className={"flex flex-col justify-center items-center"}>
       <Signup />
     </Container>
   );
