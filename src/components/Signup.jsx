1120694adc7d6e17b26da047420c578b5d37de50
Saumya Nayak <saumyo224@gmail.com>
2025-06-29 15:59:49 +0530

    refactor Signup component to enhance structure and styling

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import { Input, Button } from "./index";
+import { Link } from "react-router-dom";
 
 function Signup() {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="border-2 border-gray-600 rounded-lg m-5 p-10">
+      {/* Main heading */}
+      <div className="space-y-3">
+        <h1 className="text-4xl sm:text-5xl font-bold  text-center">
+          Join Idea Lab
+        </h1>
+        <h2 className="text-xl text-center">
+          Make the world a better place by sharing your ideas and thoughts
+        </h2>
+      </div>
 
-      <div>
-        <form action="">
-          <Input lable="Name" />
+      {/* Registration form */}
+      <div className="my-5 ">
+        <form action="" className="space-y-3 w-full ">
+          <Input type="text" lable="Name" />
+          <Input type="email" lable="Email" />
+          <Input type="text" lable="Phone no" />
+          <Input type="password" lable="Password" />
+          <Input type="password" lable="Confirm password" />
+          <Button title="Register" type="submit" />
         </form>
       </div>
+      <p className="text-center text-lg">Already registered?</p>
+
+      <Link to={"/login"}>
+        <p className="text-center text-gray-500 text-lg hover:text-gray-700 hover:cursor-pointer">
+          Login
+        </p>
+      </Link>
     </div>
   );
 }
