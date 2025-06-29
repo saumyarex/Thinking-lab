057dc36a8d1ed8e613123b0b6b7ac47adcd6e75d
Saumya Nayak <saumyo224@gmail.com>
2025-06-29 13:23:30 +0530

    enhance Container component for full-height and centered layout

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Container({ children }) {
-  return <div className="w-full ">{children}</div>;
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center ">
+      {children}
+    </div>
+  );
 }
 
 export default Container;
