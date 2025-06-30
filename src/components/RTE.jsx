20d816b6085b5b12a0db613ed6d39332adf66221
Saumya Nayak <saumyo224@gmail.com>
2025-06-30 20:01:28 +0530

    feat: add RTE component for rich text editing with form integration

---
+++ src/components/RTE.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
+
+function RTE({ label, name, control, defaultValue = "" }) {
+  const id = React.useId();
+  return (
+    <div>
+      {label && (
+        <label htmlFor={id} className="m-1 mb-4 p-2 font-medium ">
+          {label}{" "}
+        </label>
+      )}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue}
+        rules={{ required: "Required" }}
+        render={({ field: { onChange } }) => (
+          <Editor
+            apiKey={String(import.meta.env.VITE_TinyMCE_API_KEY)}
+            onEditorChange={onChange}
+            initialValue={defaultValue || "<p>Write your blog here.</p>"}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | " +
+                "bold italic forecolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+          />
+        )}
+      />
+    </div>
+  );
+}
+
+export default RTE;
