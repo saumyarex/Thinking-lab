4e8685cb8f948593b16e04f7b717da6fbb0895b9
Saumya Nayak <saumyo224@gmail.com>
2025-08-23 20:56:43 +0530

    refactor: remove unused name prop from SelectInput component

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-function SelectInput(
-  { label, name, options = [], classname = "", ...props },
-  ref
-) {
+function SelectInput({ label, options = [], classname = "", ...props }, ref) {
   const id = React.useId();
 
   return (
@@ -14,14 +11,13 @@ function SelectInput(
         </label>
       )}
       <select
-        name={name}
         id={id}
         ref={ref}
         {...props}
         className={`border-2 mt-3 border-gray-200 p-2 rounded-lg ${classname} `}
       >
         {options.map((option, id) => (
-          <option value={option.toLowerCase()} key={id}>
+          <option value={option} key={id}>
             {option}
           </option>
         ))}
