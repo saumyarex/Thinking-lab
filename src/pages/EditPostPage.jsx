a6a63febb10b232b3e0c08084061054429377270
Saumya Nayak <saumyo224@gmail.com>
2025-08-27 00:42:41 +0530

    added fetch post details functionality to EditPostPage

--- src/pages/EditPostPage.jsx
+++ src/pages/EditPostPage.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PostForm } from "../components";
+import { useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+import blogPostServices from "../appwrite/blogPostServices";
+import { LoaderCircle } from "lucide-react";
 
 function EditPostPage() {
-  const post = {
-    title: "BlockChain",
-    slug: "block-chain",
-    content:
-      "Blockchain is the digital gold and It will become one of the most values asset in the future",
-    excerpt: "The age of digital gold",
-    status: "Not Active",
-    tags: ["seo", "marketing", "social media", "design", "branding"],
-    category: "Video Production",
-    isFeatured: "Yes",
-  };
+  // state variables
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState({});
+
+  const { postId } = useParams();
+  console.log("Post id", postId);
+
+  useEffect(() => {
+    async function getPostDetails(postId) {
+      try {
+        const response = await blogPostServices.getPost(postId);
+        console.log("Post data", response);
+        setPost(response);
+        setLoading(false);
+      } catch (error) {
+        console.log("Post fetching error: ", error);
+        toast.error(error.message);
+        setLoading(false);
+      }
+    }
+
+    getPostDetails(postId);
+  }, [postId]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <LoaderCircle className="animate-spin" />
+      </div>
+    );
+  }
   return (
     <div>
       <PostForm post={post} />
