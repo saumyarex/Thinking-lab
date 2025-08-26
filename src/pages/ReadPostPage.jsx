6f7225c239eb14458c8a590dd7603e8ca8709a04
Saumya Nayak <saumyo224@gmail.com>
2025-08-26 18:59:10 +0530

    added ReadPostPage

---
+++ src/pages/ReadPostPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Toaster } from "react-hot-toast";
+
+function ReadPostPage() {
+  return (
+    <div>
+      ReadPostPage
+      <Toaster position="bottom-center" />
+    </div>
+  );
+}
+
+export default ReadPostPage;
