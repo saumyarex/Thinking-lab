a339375f374d32bd57d8f6c05c4d474cf67ff403
Saumya Nayak <saumyo224@gmail.com>
2025-08-28 17:42:20 +0530

    removed unnecessary imports

--- src/pages/EditPostPage.jsx
+++ src/pages/EditPostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { PostForm } from "../components";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
