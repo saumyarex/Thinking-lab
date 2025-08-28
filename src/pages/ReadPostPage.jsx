946ba42e906a2ac918c28d2af1b913332097614c
Saumya Nayak <saumyo224@gmail.com>
2025-08-28 17:42:51 +0530

    Read post page done

--- src/pages/ReadPostPage.jsx
+++ src/pages/ReadPostPage.jsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { PostForm } from "../components";
+import { useParams } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import blogPostServices from "../appwrite/blogPostServices";
+import { LoaderCircle } from "lucide-react";
+import { ReadPost } from "../components";
 
 function ReadPostPage() {
+  // state variables
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState({});
+
+  const { postId } = useParams();
+
+  useEffect(() => {
+    async function getPostDetails(postId) {
+      try {
+        const response = await blogPostServices.getPost(postId);
+        setPost(response);
+        setLoading(false);
+      } catch (error) {
+        console.log("Post fetching error: ", error);
+        toast.error(error.message);
+        setLoading(false);
+      }
+    }
+
+    getPostDetails(postId);
+  }, [postId]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <LoaderCircle className="animate-spin" />
+      </div>
+    );
+  }
   return (
     <div>
-      ReadPostPage
+      <ReadPost post={post} />
       <Toaster position="bottom-center" />
     </div>
   );
