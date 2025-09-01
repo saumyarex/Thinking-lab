24d2016c7381f0d7cc3f62fc8cd61e1b9bffd9ec
Saumya Nayak <saumyo224@gmail.com>
2025-09-01 19:09:36 +0530

    added userDetails data point to inital state

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   status: false,
   userData: null,
+  userDetails: null,
 };
 
 const authSlice = createSlice({
@@ -17,9 +18,12 @@ const authSlice = createSlice({
       state.userData = null;
       state.status = false;
     },
+    setUserDetails: (state, action) => {
+      state.userDetails = action.payload;
+    },
   },
 });
 
-export const { login, logout } = authSlice.actions;
+export const { login, logout, setUserDetails } = authSlice.actions;
 
 export default authSlice.reducer;
