4fe6ab277edd764c571273cf6d99193833164ea6
Saumya Nayak <saumyo224@gmail.com>
2025-09-01 19:07:30 +0530

    profile header for profile page

---
+++ src/components/ProfileHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ProfileHeader({ userDetails }) {
+  console.log("userDetails", userDetails);
+  return (
+    <div className="mt-10 px-5 rounded-lg py-10 bg-[#E2AD28]">
+      <div>
+        <h1 className="sm:text-5xl text-4xl">
+          {userDetails.firstName} {userDetails.lastName}
+        </h1>
+        <h2 className="sm:text-2xl ml-2">{userDetails.username}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileHeader;
