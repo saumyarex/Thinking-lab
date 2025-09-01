e0dbe3477b33f77ad28aacf6196fbfa51ace0ce2
Saumya Nayak <saumyo224@gmail.com>
2025-09-01 14:10:23 +0530

    added links to post and user

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,46 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-function Card({ title, date, author, imageSrc, className = "" }) {
+function Card({
+  title,
+  date,
+  author,
+  imageSrc,
+  className = "",
+  slug,
+  postId,
+  userId,
+  username,
+}) {
   return (
     <div className={`m-5 ${className} `}>
-      {/* Post main image */}
-      <div className="">
-        <img
-          src={imageSrc}
-          alt={title}
-          className="rounded-2xl shadow-2xl w-lg object-cover hover:cursor-pointer h-48"
-        />
-      </div>
+      <Link to={`/post/${slug}/${postId}`}>
+        {/* Post main image */}
+        <div className="">
+          <img
+            src={imageSrc}
+            alt={title}
+            className="rounded-2xl shadow-2xl w-lg object-cover hover:cursor-pointer h-48"
+          />
+        </div>
 
-      {/* Post info */}
-      <div className="mt-6 ">
-        {/* title */}
-        <span className="text-base sm:text-lg block mb-2 hover:cursor-pointer">
-          {title}
-        </span>
+        {/* Post info */}
+        <div className="mt-6 ">
+          {/* title */}
+          <span className="text-base sm:text-lg block mb-2 hover:cursor-pointer">
+            {title}
+          </span>
 
-        {/* date */}
-        <span>{date} </span>
-
-        {/* author name */}
+          {/* date */}
+          <span>{date} </span>
+        </div>
+      </Link>
+      {/* author name */}
+      <Link to={`/user/${username}/${userId}`}>
         <span className="text-blue-400 cursor-pointer hover:text-blue-600">
           {" "}
           by {author}
         </span>
-      </div>
+      </Link>
     </div>
   );
 }
