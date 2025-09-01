c991ce580bb4636ac9c2fcc77bd4791dec658c88
Saumya Nayak <saumyo224@gmail.com>
2025-09-01 14:10:13 +0530

    updated route to read post

--- src/main.jsx
+++ src/main.jsx
@@ -47,7 +47,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="user-update" element={<EditUserDetailsPage />} />
           <Route path="add-post" element={<AddPostPage />} />
           <Route path="edit-post/:postId" element={<EditPostPage />} />
-          <Route path="post/:postId" element={<ReadPostPage />} />
+          <Route path="post/:slug/:postId" element={<ReadPostPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
