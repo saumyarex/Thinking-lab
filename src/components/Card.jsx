7a4643e1a44a3eba796815e05dd8989eb91338b5
Saumya Nayak <saumyo224@gmail.com>
2025-09-02 16:27:58 +0530

    added delete & edit menu

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { EllipsisVertical, X } from "lucide-react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  setDeleteMenuActive,
+  setDeletePostId,
+  setDeleteImageId,
+} from "../store/postsSlice";
 
 function Card({
   title,
@@ -8,32 +17,78 @@ function Card({
   className = "",
   slug,
   postId,
+  imageId,
   userId,
   username,
 }) {
+  const dispatch = useDispatch();
+  const { userID } = useParams();
+  const userData = useSelector((state) => state.auth.userData);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
   return (
     <div className={`m-5 ${className} `}>
-      <Link to={`/post/${slug}/${postId}`}>
-        {/* Post main image */}
-        <div className="">
+      {/* Post main image */}
+      <div className="relative">
+        <Link to={`/post/${slug}/${postId}`}>
           <img
             src={imageSrc}
             alt={title}
             className="rounded-2xl shadow-2xl w-lg object-cover hover:cursor-pointer h-48"
           />
-        </div>
+        </Link>
+        {userID === userData?.userDetailsId && (
+          <div className=" bg-gray-200/50  absolute top-0 right-0 rounded-2xl cursor-pointer flex flex-col">
+            <EllipsisVertical
+              className={`self-end m-2 active:size-7 ${
+                isMenuActive ? "hidden" : "block"
+              }`}
+              onClick={() => setIsMenuActive(!isMenuActive)}
+            />
+            <X
+              className={`self-end m-2 active:size-7 ${
+                isMenuActive ? "block" : "hidden"
+              }`}
+              onClick={() => setIsMenuActive(!isMenuActive)}
+            />
+            {/* Menu options */}
+            {isMenuActive && (
+              <div className={` flex flex-col`}>
+                <Link to={`/edit-post/${postId}`}>
+                  <button className="hover:bg-gray-200 active:bg-gray-300 py-1 rounded-2xl px-5 w-full">
+                    Edit blog
+                  </button>
+                </Link>
 
-        {/* Post info */}
-        <div className="mt-6 ">
-          {/* title */}
+                <button
+                  className="hover:bg-gray-200 active:bg-gray-300 py-1 rounded-2xl px-5"
+                  onClick={() => {
+                    setIsMenuActive(!isMenuActive);
+                    dispatch(setDeleteMenuActive());
+                    dispatch(setDeletePostId(postId));
+                    dispatch(setDeleteImageId(imageId));
+                  }}
+                >
+                  Delete blog
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+
+      {/* Post info */}
+      <div className="mt-6 ">
+        {/* title */}
+        <Link to={`/post/${slug}/${postId}`}>
           <span className="text-base sm:text-lg block mb-2 hover:cursor-pointer">
             {title}
           </span>
+        </Link>
 
-          {/* date */}
-          <span>{date} </span>
-        </div>
-      </Link>
+        {/* date */}
+        <span>{date} </span>
+      </div>
       {/* author name */}
       <Link to={`/user/${username}/${userId}`}>
         <span className="text-blue-400 cursor-pointer hover:text-blue-600">
