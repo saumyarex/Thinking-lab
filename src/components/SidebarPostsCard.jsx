63481cffbb1d7640a75d1ab3ef67c112a28a22f7
Saumya Nayak <saumyo224@gmail.com>
2025-09-03 01:00:17 +0530

    added arguments to get data from parent component

--- src/components/SidebarPostsCard.jsx
+++ src/components/SidebarPostsCard.jsx
@@ -1,32 +1,47 @@
-import React from "react";
-
-function SidebarPostsCard({ title, date, author, imageSrc, className = "" }) {
+import { Link } from "react-router-dom";
+function SidebarPostsCard({
+  title,
+  date,
+  author,
+  imageSrc,
+  slug,
+  postId,
+  userId,
+  username,
+  className = "",
+}) {
   return (
     <div className={`mt-6 ${className} flex md:flex-wrap gap-2 w-full`}>
-      {/* Post main image */}
-      <div className="min-w-32 min-h-32 ">
-        <img
-          src={imageSrc}
-          alt={title}
-          className="rounded-2xl object-cover shadow-2xl hover:cursor-pointer w-32 h-32"
-        />
-      </div>
+      <Link to={`/post/${slug}/${postId}`}>
+        {/* Post main image */}
+        <div className="min-w-32 min-h-32 ">
+          <img
+            src={imageSrc}
+            alt={title}
+            className="rounded-2xl object-cover shadow-2xl hover:cursor-pointer w-32 h-32"
+          />
+        </div>
+      </Link>
 
       {/* Post info */}
       <div className=" md:w-40 min-w-30 mx-3">
-        {/* date */}
-        <span className="text-xs">{date} </span>
+        <Link to={`/post/${slug}/${postId}`}>
+          {/* date */}
+          <span className="text-xs">{date} </span>
 
-        {/* title */}
-        <span className=" block hover:cursor-pointer text-base leading-5 ">
-          {title}
-        </span>
+          {/* title */}
+          <span className=" block hover:cursor-pointer text-base leading-5 ">
+            {title}
+          </span>
+        </Link>
 
         {/* author name */}
-        <span className="text-blue-400 cursor-pointer hover:text-blue-600 text-xs">
-          {" "}
-          by {author}
-        </span>
+        <Link to={`/user/${username}/${userId}`}>
+          <span className="text-blue-400 cursor-pointer hover:text-blue-600 text-xs">
+            {" "}
+            by {author}
+          </span>
+        </Link>
       </div>
     </div>
   );
