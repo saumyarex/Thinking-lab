00db75d2abc18a7593aab1c072004eed97c7876b
Saumya Nayak <saumyo224@gmail.com>
2025-09-05 13:55:29 +0530

    added route protection

--- src/main.jsx
+++ src/main.jsx
@@ -2,43 +2,89 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import HomePage from "./pages/HomePage.jsx";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignupPage from "./pages/SignupPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import AddPostPage from "./pages/AddPostPage.jsx";
 import EditPostPage from "./pages/EditPostPage.jsx";
 import EditUserDetailsPage from "./pages/EditUserDetailsPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import ReadPostPage from "./pages/ReadPostPage.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AuthLayout from "./components/AuthLayout.jsx";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     Component: <App />,
-//     children: [
-//       {
-//         path: "/",
-//         Component: <HomePage />,
-//       },
-//       {
-//         path: "/register",
-//         Component: <SignupPage />,
-//       },
-//       {
-//         path: "/login",
-//         Component: <LoginPage />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: "",
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "login",
+        element: (
+          <AuthLayout authentication={false}>
+            <LoginPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "register",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignupPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "user/:username/:userID",
+        element: <ProfilePage />,
+      },
+      {
+        path: "user-update",
+        element: (
+          <AuthLayout authentication={true}>
+            <EditUserDetailsPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "add-post",
+        element: (
+          <AuthLayout authentication={true}>
+            <AddPostPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "edit-post/:postId",
+        element: (
+          <AuthLayout authentication={true}>
+            <EditPostPage />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "post/:slug/:postId",
+        element: <ReadPostPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
+
+{
+  /* <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="register" element={<SignupPage />} />
@@ -49,7 +95,5 @@ createRoot(document.getElementById("root")).render(
           <Route path="edit-post/:postId" element={<EditPostPage />} />
           <Route path="post/:slug/:postId" element={<ReadPostPage />} />
         </Routes>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
-);
+      </BrowserRouter> */
+}
